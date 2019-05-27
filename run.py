9dc66a51cf213e288ba836e5bf3f4d7ef40891a9
kunal <kdkunal.94@gmail.com>
2019-05-27 13:49:43 +0530

    (Project intialised) : Created basic structure for the solar inspection pipeline

---
+++ run.py
@@ -0,0 +1,2 @@
+# User executes this >
+# Run app, process the results and store into the output folder : default >
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/galactica/Galactica/Production/galactica_solar_inspection/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
